d2c08651bdfa53b6343bd5ed1b26136d7ef58827
Matteo <matteobruziches006@icloud.com>
2026-03-05 20:38:18 +0100

    secondo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 // Vsechno nejlepsi
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Puzzle from "./Puzzle";
 import Win from "./Win";
 
 export default function App() {
   return(
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Puzzle/>} />
         <Route path="/Vsechno-Nejlepsi" element={<Win/>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
